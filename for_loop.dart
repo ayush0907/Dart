5b4803dfbb70ddd3db2ebccab92043e1ca86ccc9
Ayush Raghuwanshi <94039368+ayush0907@users.noreply.github.com>
2023-02-19 18:13:53 +0530

    Create for_loop.dart

---
+++ for_loop.dart
@@ -0,0 +1,20 @@
+void main() {
+  
+  //For loop
+   print("Even Numbers between 1 to 20 :");
+  for(var i=1;i<=20;i++)
+  {
+      if(i%2==0)
+    {
+        print(i);
+    }
+  }
+  
+ // For in....loop
+    
+ List days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  for(var d in days)
+  {
+    print(d);
+  }
+}
