e2c2bafb92ee7c4b89793be513f01b7212df626d
Ayush Raghuwanshi <94039368+ayush0907@users.noreply.github.com>
2023-02-19 14:56:41 +0530

    Create variables.dart

---
+++ variables.dart
@@ -0,0 +1,21 @@
+void main() {
+  String name="Ayush"; 
+  var lastname="Raghuwanshi";
+  print(name);
+  print(lastname);
+  
+  int age=22;
+  var dob=2000;
+  print(age);
+  print(dob);
+  
+  double percent=89.3;
+  var percentage=78.6;
+  print(percent);
+  print(percentage);
+  
+  bool isalive=true;
+  var isgood=false;
+  print(isalive);
+  print(isgood);
+  }
