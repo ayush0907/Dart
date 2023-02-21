b2016d3ced5101436136d97e791f868cf2f298b2
Ayush Raghuwanshi <94039368+ayush0907@users.noreply.github.com>
2023-02-21 12:34:43 +0530

    Update Set.dart

--- Set.dart
+++ Set.dart
@@ -1,6 +1,10 @@
 void main() {
   var numberSet = {35, 67, 33, 89, 232, 77};
   numberSet.add(35); //It will insert duplicate elements
+  numberSet.remove(67); //It will remove element
+  print("Length of Set is ${numberSet.length}"); //it will give length of set
+  print(numberSet.isEmpty); //to check whether set is empty or not
+  // numberSet.clear();   //to empty set
 
   var number = Set.from([33, 32, 44]); //Using from list
   number.add(70);
@@ -9,13 +13,13 @@ void main() {
   s1.add("Ayush");
   s1.add("Anuj");
 
-  s1.forEach((n) => print(n)); //By using lambda expression
+  for (int n in numberSet) {
+    print(n); //By using for...in loop
+  }
   print("");
 
   number.forEach((n) => print(n)); //By using lambda expression
   print("");
 
-  for (int n in numberSet) {
-    print(n); //By using for...in loop
-  }
+  s1.forEach((n) => print(n)); //By using lambda expression
 }
